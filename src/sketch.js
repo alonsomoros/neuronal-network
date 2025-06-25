446a7a2b44d050666ff9b9cdb1f0386b58141f84
Alon <alonso.moros@alumnos.upm.es>
2025-06-25 23:39:15 +0200

    Añadidas pruebas para operaciones y setup para la prueba de la red neuronal

--- src/sketch.js
+++ src/sketch.js
@@ -1,71 +1,112 @@
 var brain;
 
 function setup() {
-
-    // << Multiplicación de matrices por número y matriz en variable >>
-    // let a = new Matrix(2, 3);
-    // let b = new Matrix(3, 2);
-    //
-    // a.randomizar();
-    // b.randomizar();
-    //
-    // console.table(a.datos);
-    // console.table(b.datos);
-    //
-    // let multiplicacionPorNumero = Matrix.prototype.multiplicarMatrices(a,2);
-    // let multiplicacionPorMatriz = Matrix.prototype.multiplicarMatrices(a,b);
-    // console.table(multiplicacionPorNumero.datos);
-    // console.table(multiplicacionPorMatriz.datos);
-
-    // << Multiplicación de matrices en propia instancia >>
-    // let d = new Matrix(3, 3);
-    // let e = new Matrix(3, 3);
-    // d.initPrueba();
-    // e.initPrueba();
-    // console.table(d.datos);
-    // console.table(e.datos);
-    //
-    // d.multiplicar(e);
-    // console.table(d.datos);
-
-    // << Multiplicación de matriz por un número en propia instancia >>
-    // let f = new Matrix(3, 3);
-    // f.initPrueba();
-    // console.table(f.datos);
-    //
-    // f.multiplicar(2)
-    // console.table(f.datos);
-
-
-    // << Suma de matrices en variable >>
-    // let g = new Matrix(3, 3);
-    // let h = new Matrix(3, 3);
-    // g.initPrueba();
-    // h.initPrueba();
-    // console.table(g.datos);
-    // console.table(h.datos);
-    // let suma = Matrix.prototype.sumarMatrices(g, h);
-    // console.table(suma.datos);
-
-    // << Suma de matrices en propia instancia >>
-    // let i = new Matrix(3, 3);
-    // let j = new Matrix(3, 3);
-    // i.initPrueba();
-    // j.initPrueba();
-    // console.table(i.datos);
-    // console.table(j.datos);
-    // i.sumar(j);
-    // console.table(i.datos);
-
-    // << Suma de matriz por un número en propia instancia >>
-    // let k = new Matrix(3, 3);
-    // k.initPrueba();
-    // console.table(k.datos);
-    // k.sumar(2);
-    // console.table(k.datos);
-
+    // pruebaMatrices();
+    pruebaRedNeuronal();
 }
 
 function draw() {
 
+}
+
+function duplicar(x) {
+    return x * 2;
+}
+
+function duplicarDiagonal(x, fila, columna) {
+    if (fila === columna) {
+        return x * 2;
+    }
+}
+
+function pruebaRedNeuronal() {
+    let redNeuronal = new RedNeuronal(4, 2, 1);
+    let entradas = [1, 0, 0, 1];
+    let salidas = redNeuronal.feedforward(entradas);
+    console.log("Para entradas: " + entradas + ", la salida es: " + salidas);
+}
+
+function pruebaMatrices() {
+    // << Multiplicación de matrices por número y matriz en variable >>
+    let a = new Matrix(4, 1);
+    let b = new Matrix(1, 3);
+
+    a.randomizar();
+    b.randomizar();
+    a.print();
+    b.print();
+
+    let multiplicacionPorNumero = Matrix.multiplicarMatrices(a, 2);
+    let multiplicacionPorMatriz = Matrix.multiplicarMatrices(a, b);
+    multiplicacionPorNumero.print();
+    multiplicacionPorMatriz.print();
+
+    // << Multiplicación de matrices en propia instancia >>
+    let d = new Matrix(4, 1);
+    let e = new Matrix(1, 3);
+    d.randomizar();
+    e.randomizar();
+    d.print();
+    e.print();
+
+    d.multiplicar(e);
+    d.print();
+
+    // << Multiplicación de matriz por un número en propia instancia >>
+    let f = new Matrix(3, 3);
+    f.randomizar();
+    f.print();
+
+    f.multiplicar(2)
+    f.print();
+
+
+    // << Suma de matrices en variable >>
+    let g = new Matrix(5, 4);
+    let h = new Matrix(5, 4);
+    g.randomizar();
+    h.randomizar();
+    g.print();
+    h.print();
+    let suma = Matrix.sumarMatrices(g, h);
+    suma.print();
+
+    // << Suma de matrices en propia instancia >>
+    let i = new Matrix(4, 4);
+    let j = new Matrix(4, 4);
+    i.randomizar();
+    j.randomizar();
+    i.print();
+    j.print();
+    i.sumar(j);
+    i.print();
+
+    // << Suma de matriz por un número en propia instancia >>
+    let k = new Matrix(2, 2);
+    k.randomizar();
+    k.print();
+    k.sumar(10);
+    k.print();
+
+    // << Transposición de matrices en variable >>
+    let l = new Matrix(2, 3);
+    l.randomizar();
+    l.print();
+
+    let traspuestaVariable = Matrix.transponerMatriz(l);
+    traspuestaVariable.print();
+
+    // << Transposición de matrices en propia instancia >>
+    let m = new Matrix(2, 3);
+    m.randomizar();
+    m.print();
+
+    let traspuestaPropia = Matrix.transponerMatriz(m);
+    traspuestaPropia.print();
+
+    let duplicada = new Matrix(2, 2);
+    duplicada.randomizar();
+    duplicada.print();
+    duplicada.map(duplicar);
+    duplicada.print();
 }
