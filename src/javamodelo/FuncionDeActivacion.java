26a12f592ca8874c39db2c92768ed0a9747b95a7
Alon <alonso.moros@alumnos.upm.es>
2025-07-04 18:45:25 +0200

    Añadido nombre de Funcion De Activación para tracking y display

--- src/javamodelo/FuncionDeActivacion.java
+++ src/javamodelo/FuncionDeActivacion.java
@@ -3,10 +3,12 @@ package javamodelo;
 import java.util.function.Function;
 
 public class FuncionDeActivacion<T extends Number> {
+    private final String nombre;
     private final Function<T,T> function;
     private final Function<T,T> derivative;
 
-    public FuncionDeActivacion(Function<T,T> function, Function<T,T> derivative) {
+    public FuncionDeActivacion(String nombre, Function<T,T> function, Function<T,T> derivative) {
+        this.nombre = nombre;
         this.function = function;
         this.derivative = derivative;
     }
@@ -17,5 +19,9 @@ public class FuncionDeActivacion<T extends Number> {
 
     public Function<T,T> getDerivative() {
         return derivative;
+    }
+
+    public String getNombre() {
+        return this.nombre;
     }
 }
