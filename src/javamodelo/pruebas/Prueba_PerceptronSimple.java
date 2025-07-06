80def8533a9014aa10dea41ebef4f3f760bd586c
Alon <alonso.moros@alumnos.upm.es>
2025-07-06 16:32:23 +0200

    Añadida clase para probar problemas lineales para el perceptron simple

---
+++ src/javamodelo/pruebas/Prueba_PerceptronSimple.java
@@ -0,0 +1,12 @@
+package javamodelo.pruebas;
+
+import javamodelo.Punto;
+
+public class Prueba_PerceptronSimple {
+
+    public final int NUM_PUNTOS = 100;
+    public int EPOCHS = 1000;
+    public final Punto[] PUNTOS = new Punto[NUM_PUNTOS];
+    public final float TASA_APRENDIZAJE = 0.1f;
+
+}
