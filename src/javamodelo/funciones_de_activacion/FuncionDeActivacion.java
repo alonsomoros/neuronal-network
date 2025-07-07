539732e6ee21ebfc420fe374eec8d98270836f0d
Alon <alonso.moros@alumnos.upm.es>
2025-07-08 01:29:10 +0200

    Cambiado tipo a Float[] para gestionar todo tipo de entradas y salidas para las funciones

--- src/javamodelo/funciones_de_activacion/FuncionDeActivacion.java
+++ src/javamodelo/funciones_de_activacion/FuncionDeActivacion.java
@@ -4,20 +4,20 @@ import java.util.function.Function;
 
 public class FuncionDeActivacion<T extends Number> {
     private final String nombre;
-    private final Function<T,T> function;
-    private final Function<T,T> derivative;
+    private final Function<T[],T[]> function;
+    private final Function<T[],T[]> derivative;
 
-    public FuncionDeActivacion(String nombre, Function<T,T> function, Function<T,T> derivative) {
+    public FuncionDeActivacion(String nombre, Function<T[],T[]> function, Function<T[],T[]> derivative) {
         this.nombre = nombre;
         this.function = function;
         this.derivative = derivative;
     }
 
-    public Function<T,T> getFunction() {
+    public Function<T[],T[]> getFunction() {
         return function;
     }
 
-    public Function<T,T> getDerivative() {
+    public Function<T[],T[]> getDerivative() {
         return derivative;
     }
 
