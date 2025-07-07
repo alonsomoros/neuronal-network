e957758dc0260cd6a235e515f9c343cc606608d4
Alon <alonso.moros@alumnos.upm.es>
2025-07-07 13:49:57 +0200

    Añadido método para cargar el DATASET de la XOR para concordancia de DATASETS

--- src/javamodelo/utils/Helper.java
+++ src/javamodelo/utils/Helper.java
@@ -1,6 +1,8 @@
 package javamodelo.utils;
 
+import javamodelo.pruebas.Data;
 import javamodelo.pruebas.iris.IrisData;
+import javamodelo.pruebas.xor.XORData;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -9,8 +11,8 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Helper {
-    public static ArrayList<IrisData> getIrisDataset(String ruta) {
-        ArrayList<IrisData> dataset = new ArrayList<>();
+    public static ArrayList<Data> getIrisDataset(String ruta) {
+        ArrayList<Data> dataset = new ArrayList<>();
         File file = new File(ruta);
         try {
             Scanner scanner = new Scanner(file);
@@ -18,7 +20,7 @@ public class Helper {
                 String linea = scanner.nextLine();
                 String[] partes = linea.split(";");
                 String nombre = partes[4];
-                float[] caracteristicas = new float[4];
+                Float[] caracteristicas = new Float[4];
                 for (int i = 0; i < partes.length - 1; i++) {
                     caracteristicas[i] = Float.parseFloat((partes[i]));
                 }
@@ -28,5 +30,14 @@ public class Helper {
             exception.getMessage();
         }
         return dataset;
+    }
+
+    public static ArrayList<Data> getXORDataset() {
+        ArrayList<Data> datos_XOR = new ArrayList<>();
+        datos_XOR.add(new XORData(new Float[]{0f, 0f}, new Float[]{0f}));
+        datos_XOR.add(new XORData(new Float[]{0f, 1f}, new Float[]{1f}));
+        datos_XOR.add(new XORData(new Float[]{1f, 0f}, new Float[]{1f}));
+        datos_XOR.add(new XORData(new Float[]{1f, 1f}, new Float[]{0f}));
+        return datos_XOR;
     }
 }
