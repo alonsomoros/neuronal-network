a7cba6b052914f1da09f6d2fb78a8b265ed1088c
Alon <alonso.moros@alumnos.upm.es>
2025-07-07 14:34:57 +0200

    Implementada en las Pruebas de la IRIS las clases padres

--- src/javamodelo/pruebas/iris/IrisData.java
+++ src/javamodelo/pruebas/iris/IrisData.java
@@ -1,20 +1,38 @@
 package javamodelo.pruebas.iris;
 
-public class IrisData {
-    public float[] caracteristicas;
+import javamodelo.pruebas.Data;
+
+public class IrisData extends Data {
+    public Float[] caracteristicas;
     public String nombre;
 
-    public IrisData(float[] caracteristicas, String nombre) {
-        this.caracteristicas = caracteristicas;
+    public IrisData(Float[] caracteristicas, String nombre) {
+        super(caracteristicas, labelToOneHot(nombre));
         this.nombre = nombre;
     }
 
-    public static double[] labelToOneHot(String label) {
+    public static Float[] labelToOneHot(String label) {
         switch (label) {
-            case "setosa": return new double[]{1, 0, 0};
-            case "versicolor": return new double[]{0, 1, 0};
-            case "virginica": return new double[]{0, 0, 1};
+            case "setosa": return new Float[]{1f, 0f, 0f};
+            case "versicolor": return new Float[]{0f, 1f, 0f};
+            case "virginica": return new Float[]{0f, 0f, 1f};
             default: throw new IllegalArgumentException("Etiqueta desconocida: " + label);
         }
+    }
+
+    public Float[] getCaracteristicas() {
+        return caracteristicas;
+    }
+
+    public void setCaracteristicas(Float[] caracteristicas) {
+        this.caracteristicas = caracteristicas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 }
--- src/javamodelo/pruebas/iris/Prueba_IRIS.java
+++ src/javamodelo/pruebas/iris/Prueba_IRIS.java
@@ -1,18 +1,38 @@
 package javamodelo.pruebas.iris;
 
+import javamodelo.pruebas.Data;
+import javamodelo.pruebas.Prueba;
+import javamodelo.pruebas.xor.RedNeuronal;
 import javamodelo.utils.Helper;
+import processing.core.PApplet;
 
+import java.awt.*;
 import java.util.ArrayList;
 
-public class Prueba_IRIS {
-    public final ArrayList<IrisData> IRIS_DATASET = Helper.getIrisDataset("rec/Iris_dataset.csv");
-    public final ArrayList<IrisData> DATOS_ENTRENAMIENTO;
-    public final ArrayList<IrisData> DATOS_TEST;
+public class Prueba_IRIS extends Prueba {
 
     public Prueba_IRIS(int porcentajeEntrenamiento) {
-        int porcentaje = IRIS_DATASET.size() * porcentajeEntrenamiento / 100;
-        DATOS_ENTRENAMIENTO = new ArrayList<>(IRIS_DATASET.subList(0, porcentaje));
-        DATOS_TEST = new ArrayList<>(IRIS_DATASET.subList(porcentaje, IRIS_DATASET.size()));
+        super(porcentajeEntrenamiento);
     }
 
+    public void draw(PApplet pApplet) {
+        if (getRedesNeuronales().isEmpty()) return;
+
+        // Cálculos para dimensiones de las gráficas
+        int columnasDeGraficas = (int) Math.round(Math.sqrt(this.redesNeuronales.size() + 1));                      // Porciones de columnas
+        int filasDeGraficas = (int) Math.ceil((double) (this.redesNeuronales.size() + 1) / columnasDeGraficas);     // Porciones de filas
+        int anchoCuadrante = pApplet.width / columnasDeGraficas;                                                    // Ancho de cada cuadrante
+        int altoCuadrante = pApplet.height / filasDeGraficas;
+
+        Rectangle coords_grafica_error_epoch = new Rectangle(0, 0, anchoCuadrante, altoCuadrante);
+
+        for (RedNeuronal redNeuronal : redesNeuronales) {
+            actualizarYDibujarRed(pApplet, redNeuronal, coords_grafica_error_epoch);
+        }
+    }
+
+    private void actualizarYDibujarRed(PApplet pApplet, RedNeuronal redNeuronal, Rectangle coordenadasGrafica) {
+        redNeuronal.actualizar(pApplet);
+        redNeuronal.dibujarValoresGrafica(pApplet, coordenadasGrafica, redNeuronal.getEpochs(), redNeuronal.getErroresEpochs());
+    }
 }
